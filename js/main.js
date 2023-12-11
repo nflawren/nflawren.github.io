bbd7be7e2fa1a0b291b410a8120c6e9277507ae3
Nicole Lawrence <67914648+nflawren@users.noreply.github.com>
2023-12-11 13:33:23 -0500

    nav bugfix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ Vue.component('custom-nav', {
     template: `
         <nav @mouseover="hover()" @mouseout="noHover()">
             <a href="index.html" :class="{selected: isSelected('index')}">Home</a>
-            <a href="aboutMe.html" :class="{selected: isSelected('about')}">About Me</a>
+            <a href="aboutme.html" :class="{selected: isSelected('about')}">About Me</a>
             <a href="resume.html" :class="{selected: isSelected('resume')}">Resume</a>
             <a href="funfacts.html" :class="{selected: isSelected('facts')}">Fun Facts</a>
             <a href="photography.html" :class="{selected: isSelected('photo')}">Photography</a>
